a40d36cfdc9fa5bbcd5dac831f192d9c2386539e
Nataliia Shelikhova <105817686+NataliiaShelikhova@users.noreply.github.com>
2022-09-04 14:23:19 +0300

    Include a link to my GitHub repository

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
   return (
     <div className="App">
       <Forecast />
+      <p>
+        <a href='https://github.com/NataliiaShelikhova/weather-react' target="_blank" rel="noreferrer">Open-source code</a> by <a href='https://astounding-chaja-60425e.netlify.app' target="_blank" rel="noreferrer">Nata Shelikhova</a>
+      </p>
     </div>
   );
 }
